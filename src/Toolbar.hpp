a77478aaa6690d20423c94d7d9a9e3cc5a655282
ClAndrew <cmlandrews@gmail.com>
2026-09-20 07:43:54 -0700

    Implement a simple pencil, fix Windows build (#33)
    * Simple pencil functionality
    
    * Fix Windows build
    
    * Make checkbox logic more manageable
    
    * Update build instructions

--- src/Toolbar.hpp
+++ src/Toolbar.hpp
@@ -10,6 +10,7 @@ namespace hackforge {
 
 enum class Tool
 {
+    Pencil,
     Stamp,
     AnglePen,
     PaintBucket
@@ -21,6 +22,7 @@ void OnToolbarSaveAs();
 void OnToolbarExit();
 void OnToolbarSetPenColor();
 void OnToolbarSetUIColor();
+void OnToolbarSetPencilTool();
 void OnToolbarSetStampTool();
 void OnToolbarSetAnglePenTool();
 void OnToolbarSetPaintBucketTool();
@@ -250,6 +252,7 @@ public:
     {
       TopLevelMenuItem tool;
       tool.SetLabel("Tool");
+      tool.AddChildMenuItem("Pencil", x, OnToolbarSetPencilTool);
       tool.AddChildMenuItem("Stamp", x, OnToolbarSetStampTool);
       tool.AddChildMenuItem("Angle Pen", x, OnToolbarSetAnglePenTool);
             tool.AddChildMenuItem("Bucket", x, OnToolbarSetPaintBucketTool);
@@ -357,6 +360,17 @@ public:
                                     bool checkedState) {
     m_toolbarItems[toolbarIndex].SetChildMenuItemCheckedState(
         childMenuItemIndex, checkedState);
+  }
+
+  void CheckItemAndUncheckOthers(size_t toolbarIndex, size_t childMenuItemIndex)
+  {
+      size_t numChildMenuItems = m_toolbarItems[toolbarIndex].GetNumChildMenuItems();
+
+      for (size_t i = 0; i < numChildMenuItems; ++i)
+      {
+          bool checkedState = childMenuItemIndex == i;
+          m_toolbarItems[toolbarIndex].SetChildMenuItemCheckedState(childMenuItemIndex, checkedState);
+      }
   }
 };
 
